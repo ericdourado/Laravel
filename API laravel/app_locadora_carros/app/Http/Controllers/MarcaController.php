ae155d4026f86232b8e53c9a9cc172b9242b0a7b
Eric Santos <eric.santos@codenapp.com>
2022-09-11 20:33:57 -0300

    API em Laravel

---
+++ API laravel/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Request;
+
+class MarcaController extends Controller
+{
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $marca = Marca::all();
+        $marcas = $this->marca::all();
+        return response()->json($marcas, 200);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+        $marcas = $this->marca::create($request->all());
+        return response()->json($marcas, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(["error" => "Recurso pesquisado nao existe"], 404);
+        }
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(["error" => "Impossivel realizar alteração, pois recurso não existe"], 404);
+        }
+        $marca->update($request->all());
+        return response()->json($marca, 200);
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(["error" => "Impossivel realizar a exclusao, pois recurso não existe"], 404);
+        }
+        $marca->delete();
+
+        return response()->json(["msg" => "A marca foi removida"], 201);
+    }
+}
---
+++ API laravel/app_locadora_carros/app/Models/Marca.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return [
+            "nome" => "required|unique:marcas|min:3",
+            "imagem" => "required"
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            "required" => "O campo :attribute é obrigatório",
+            "nome.unique" => "O nome da marca ja existe",
+            "nome.min" => "O nome deve ter no minimo 3 caracteres"
+        ];
+    }
+}
